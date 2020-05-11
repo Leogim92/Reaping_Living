c2bd0d0d2ec0a37a9a74021c2afecede37172a58
Leogim <n.m.santos8@gmail.com>
2020-05-11 11:52:50 +0100

    15. Prefab base done.

---
+++ Assets/SingletonTest.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SingletonTest : MonoBehaviour
+{
+    [SerializeField]AudioSource audioSource;
+    private void Awake()
+    {
+        if (FindObjectsOfType<SingletonTest>().Length > 1) Destroy(this);
+        else DontDestroyOnLoad(this);
+    }
+}
